a338ce7e3c3d6e2cd7b804a3c735624dc7212746
pwl-ev <pwl93.e@gmail.com>
2023-04-23 23:16:36 +0300

    add tagged posts

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,27 +4,34 @@ from django.core.paginator import Paginator, PageNotAnInteger,  EmptyPage
 from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
 
 # Create your views here.
 
-# def post_list(request):
-#     object_list = Post.published.all()
-#     paginator = Paginator(object_list, 3)
-#     page = request.GET.get('page')
-#     try:
-#         posts = paginator.page(page)
-#     except PageNotAnInteger:
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         posts = paginator.page(paginator.num_pages)
-        
-#     return render(request, 'blog/post/list.html', {'page': page,'posts': posts})
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
 
-class PostListView(ListView):
-    queryset = Post.published.all() # переопределяем queryset модели.
-    context_object_name = 'posts' # задаем имя которое будет передаваться в шаблон (название переменной контекста)
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+        
+    return render(request, 'blog/post/list.html', {'page': page,'posts': posts, 'tag': tag})
+
+# class PostListView(ListView):
+#     queryset = Post.published.all() # переопределяем queryset модели.
+#     context_object_name = 'posts' # задаем имя которое будет передаваться в шаблон (название переменной контекста)
+#     paginate_by = 3
+#     template_name = 'blog/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
