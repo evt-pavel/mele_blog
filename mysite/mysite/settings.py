c0afed2f61624fd8f1f7b6694d1e19063d19d670
pwl-ev <pwl93.e@gmail.com>
2023-04-27 23:30:37 +0300

    ccevert "connected the PostgreSQL"
    This reverts commit 59a045831905fce712f46ebdff8d5d411b2ea293.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blog',
-        'USER': 'blog',
-        'PASSWORD': '12345',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
